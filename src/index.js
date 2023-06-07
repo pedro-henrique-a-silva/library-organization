6475d49488d56330136b20c9369d44024cf20a05
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-06-07 15:16:07 -0300

    Requisitos 1 e 2 concluidos

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ const { books } = require('../data/library');
 // Requisito 1
 const fantasyOrScienceFiction = () => {
   // escreva seu código aqui
+  return books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
 };
 
 const oldBooksOrdered = (year) => {
   // escreva seu código aqui
+  const oldBooks = books.filter((book) => 2020 - book.releaseYear >= 60);
+  return oldBooks.sort((a, b) => a.releaseYear - b.releaseYear);
 };
 
 const booksByAuthorBirthYear = (birthYear) => {
@@ -24,6 +27,8 @@ const oldBooks = (year) => {
 const authorWith3DotsOnName = () => {
   // escreva seu código aqui
 };
+
+console.log(oldBooksOrdered(2020))
 
 module.exports = {
   fantasyOrScienceFiction,
